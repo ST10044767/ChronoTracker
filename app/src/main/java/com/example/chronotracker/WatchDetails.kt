5532f092bf8f2967db62e6959a57fd4d7b360579
Madimetja Terence Chuene <155132652+Takashi-91@users.noreply.github.com>
2024-06-10 19:05:09 +0000

    Fix gradle

--- app/src/main/java/com/example/chronotracker/WatchDetails.kt
+++ app/src/main/java/com/example/chronotracker/WatchDetails.kt
@@ -6,8 +6,8 @@ import java.io.Serializable
 data class Watch(
     val name: String,
     val color: String,
-    val movement: String,
-    val year: Int,
-    val price: Double,
+    val movement: String?,
+    val year: Int?,
+    val price: Double?,
     val imageUri: Uri? // Added image URI field
 ) : Serializable
